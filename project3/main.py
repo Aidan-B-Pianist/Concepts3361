ce24efea3816af3d262b35f687a53017dccaf8f1
aburchett <aidan@burchett.me>
2024-11-27 13:57:45 -0600

    argparser implemented, writing output directly after computation of matrix

--- project3/main.py
+++ project3/main.py
@@ -1,23 +1,36 @@
-import sys
 import math
 import argparse
+import os.path
 
+#Parse arguments for input and output
 parser = argparse.ArgumentParser(description="Project 3 Multiprocessor")
 
 parser.add_argument('-i', '--input', required=True)
 parser.add_argument('-o', '--output', required=True)
+parser.add_argument('-p', '--processor', required=False, type=int)
 
 args = parser.parse_args()
+
+if not os.path.exists(args.input):
+    parser.error("The file %s does not exist" % args.input)
+
+if not os.path.exists(os.path.join(args.output)):
+    parser.error("The directories in the file path does not exist")
+
+if args.processor is not None and args.processor <= 0:
+    parser.error("You must put a value greater than 0")
 
 #Command line arguments
 cmdline = args.input
 outputFile = args.output
+
 inputFile = open(cmdline, "r")
 
 #Do these in batches
 lowerBound = 0
 upperBound = 3
 
+#list of prime numbers
 listofPrimeNumbers = [2, 3, 5, 7, 11, 13]
 
 #Convert string file into 2d array with integers
@@ -80,14 +93,33 @@ def getAdjectCells(matrix, x_cord, y_cord):
 
 #Do the transforming
 def Compute(matrix):
+    fileOutput = open(outputFile, "w")
     for i in range(len(matrix) - 1):
         for j in range(len(matrix[0])):
             neighbors = getAdjectCells(matrix, i, j)
             # print(matrix[i][j])
             # print(neighbors)
-
+            #Do the necessary calculations established in the rulebook
             matrix[i][j] = Rules(matrix[i][j], neighbors)
+
             #write it to the file here
+            def checkCase(input):
+                if input == 2:
+                    return 'O'
+                elif input == 1:
+                    return 'o'
+                elif input == 0:
+                    return '.'
+                elif input == -1:
+                    return 'x'
+                elif input == -2:
+                    return 'X'
+            lines = [(checkCase(matrix[i][j]))]
+            output = ''.join(lines)
+            fileOutput.write(output)
+            
+        linebreaker = '\n'.join('\n')
+        fileOutput.write(linebreaker)
 
 
     return matrix
@@ -99,12 +131,6 @@ def powerOfTwo(value):
 def primeNumber(value):
     return value in listofPrimeNumbers
 
-def absolutePrimeNumber(value):
-    return abs(primeNumber(value))
-
-def powerOfTwoAbsolute(value):
-    return abs(value > 0 and math.log2(value).is_integer)
-
 def Rules(value, neighbors):
     total = sum(neighbors)
     if value == 2:
@@ -114,62 +140,38 @@ def Rules(value, neighbors):
             return 1
         return 2
     elif value == 1:
-        if total < 0:
+        if total <= 0:
             return 0
-        elif total > 8:
+        elif total >= 8:
             return 2
         return 1
     elif value == 0:
         if primeNumber(total):
             return 1
-        elif absolutePrimeNumber(total):
+        elif abs(primeNumber(total)):
             return -1
         return 0
     elif value == -1:
         if total >= 1:
             return 0
-        elif total < -8:
+        elif total <= -8:
             return -2
         return -1
     elif value == -2:
-        if powerOfTwoAbsolute(value):
+        if abs(powerOfTwo(total)):
             return 0
         elif value > -10:
             return -1
         return -2
-
     
-
-
-#Convert String to Integer
-def IntToStringArray(matrix):
-    fileOutput = open(outputFile, "w")
-    def checkCase(input):
-        if input == 2:
-            return 'O'
-        elif input == 1:
-            return 'o'
-        elif input == 0:
-            return '.'
-        elif input == -1:
-            return 'x'
-        elif input == -2:
-            return 'X'
-    
-    lines = [''.join(checkCase(num) for num in line) for line in matrix]
-    lines = '\n'.join(lines)
-    for line in lines:
-        fileOutput.write(line)
 
 
 
 
 def main():
     result = StringToIntArray(inputFile)
-    matrix = Compute(result)
+    Compute(result)
 
-    IntToStringArray(matrix)
-    
 
 
 main()
